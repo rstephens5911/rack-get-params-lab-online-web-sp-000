b3b24140e803e1ae1efcd385524cba047f99cb08
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 01:13:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -32,7 +32,10 @@ class Application
         else
           resp.write "We don't have that item!"
         end
+      else
+        resp.write "Path Not Found"
       end
+      
       resp.finish
     end
 
