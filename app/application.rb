88d269b18abe8d41ff11b23cad48547493d5a664
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 01:06:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     if req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      
+
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}"
