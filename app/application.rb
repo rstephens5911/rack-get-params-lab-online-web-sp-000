ee552800a69d8b60654db964cfdbf9afd1727c51
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 01:11:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ class Application
           @@cart << item_to_add
           resp.write "added #{item_to_add} "
         else
-          resp.write "Item not found."
+          resp.write "Path not found."
         end
       end
     resp.finish
